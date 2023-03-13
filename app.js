eb9a9e33ae244176c02beb45e6eb7f2fbafa8367
xxNecromancerx <49578340+xxNecromancerx@users.noreply.github.com>
2023-03-13 16:41:07 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+let waifuList = ["Zero Two","Hinata","Makima","Maki","Momo","Nobara","Rika"];
+
+
+
+let zeroTwoBtn = document.getElementById("zeroTwoBtn");
+
+zeroTwoBtn.addEventListener("click", function() { 
+    document.getElementById('image').setAttribute("src","02.jpg") // Changes Image src attribute to image's src.
+})
+
+let HinataBtn = document.getElementById("hinataBtn");
+
+HinataBtn.addEventListener("click", function() { 
+    document.getElementById('image').setAttribute("src","hinata.jpg") // Changes Image src attribute to image's src.
+})
+
+let makimaBtn = document.getElementById("makimaBtn");
+
+makimaBtn.addEventListener("click", function() { 
+    document.getElementById('image').setAttribute("src","makima.jpg") // Changes Image src attribute to image's src.
+})
+
+let makiBtn = document.getElementById("makiBtn");
+
+makiBtn.addEventListener("click", function() { 
+    document.getElementById('image').setAttribute("src","maki.jpg") // Changes Image src attribute to image's src.
+})
+
+let momoBtn = document.getElementById("momoBtn");
+
+momoBtn.addEventListener("click", function() { 
+    document.getElementById('image').setAttribute("src","momo.jpg") // Changes Image src attribute to image's src.
+})
+
+let nobaraBtn = document.getElementById("nobaraBtn");
+
+nobaraBtn.addEventListener("click", function() { 
+    document.getElementById('image').setAttribute("src","nobara.jpg") // Changes Image src attribute to image's src.
+})
+
+let rikaBtn = document.getElementById("rikaBtn");
+
+rikaBtn.addEventListener("click", function() { 
+    document.getElementById('image').setAttribute("src","rika.jpg") // Changes Image src attribute to image's src.
+})
